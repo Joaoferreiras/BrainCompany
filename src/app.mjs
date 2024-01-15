10a45e9b3ca7fd825c2fb9c796543b4c035ad2f4
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-15 15:22:59 -0300

    chore: configurando a conexão entre o model e o banco de dados

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,6 @@
 import express from "express"
 import routes from "./routes.mjs"
+import "./database/index.mjs"
 
 class App {
   constructor() {
---
+++ src/database/index.mjs
@@ -0,0 +1,20 @@
+import Sequelize from "sequelize"
+
+import User from "../app/models/User.mjs"
+
+import configDatabase from "../config/database.cjs"
+
+const models = [User]
+
+class Database {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.connection = new Sequelize(configDatabase)
+    models.map((model) => model.init(this.connection))
+  }
+}
+
+export default new Database()
