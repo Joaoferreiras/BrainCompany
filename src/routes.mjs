1322ccd3ac204b08754cf5e01546e15538e5c3c8
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-15 15:43:20 -0300

    test: testando conexão entre o model e o banco de dados

--- src/routes.mjs
+++ src/routes.mjs
@@ -1,9 +1,18 @@
 import { Router } from "express"
+import { v4 } from "uuid"
+import User from "./app/models/User.mjs"
 
 const routes = new Router()
 
-routes.get("/", (request, response) => {
-  return response.json("{message: Hello Brain}")
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: "João",
+    email: "test@gmail.com",
+    password_hash: "1abc23",
+  })
+
+  return response.json(user)
 })
 
 export default routes
