cf257500d06ee8e00b033658665f9f9180f4685d
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-15 21:19:43 -0300

    feat: Adicionando validação de email duplicado

--- src/app/controllers/UserController.mjs
+++ src/app/controllers/UserController.mjs
@@ -22,6 +22,15 @@ class UserController {
     }
 
     const { name, email, password_hash, admin } = request.body
+
+    const userExists = User.findOne({
+      where: { email },
+    })
+
+    if (userExists) {
+      return response.status(400).json({ error: "User already exists" })
+    }
+
     const user = await User.create({
       id: v4(),
       name,
