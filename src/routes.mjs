8d7091467ab2e6c42385ecc925e822487fcac37b
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-18 17:59:55 -0300

    chore: atualização na rota de products para enviar um arquivo usando o multer

--- src/routes.mjs
+++ src/routes.mjs
@@ -2,11 +2,14 @@ import { Router } from "express"
 import UserController from "./app/controllers/UserController.mjs"
 import SessionController from "./app/controllers/SessionController.mjs"
 import ProductController from "./app/controllers/ProductController.mjs"
+import multer from "multer"
+import multerConfig from "./config/multer.cjs"
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post("/users", UserController.store)
 routes.post("/sessions", SessionController.store)
-routes.post("/products", ProductController.store)
+routes.post("/products", upload.array("files"), ProductController.store)
 
 export default routes
