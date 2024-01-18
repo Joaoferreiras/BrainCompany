202cd4e5c5ecce7ea32b4cae65170f170b6738c8
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-17 21:14:08 -0300

    chore: criação da rota de products

--- src/routes.mjs
+++ src/routes.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import UserController from "./app/controllers/UserController.mjs"
 import SessionController from "./app/controllers/SessionController.mjs"
+import ProductController from "./app/controllers/ProductController.mjs"
 
 const routes = new Router()
 
 routes.post("/users", UserController.store)
 routes.post("/sessions", SessionController.store)
+routes.post("/products", ProductController.store)
 
 export default routes
