7192f741c9b8c3d354f8e97d448aa584d4cfa620
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-22 14:33:50 -0300

    feat: adicionando authMiddleware as rotas que precisam de autenticação

--- src/routes.mjs
+++ src/routes.mjs
@@ -4,12 +4,15 @@ import SessionController from "./app/controllers/SessionController.mjs"
 import ProductController from "./app/controllers/ProductController.mjs"
 import multer from "multer"
 import multerConfig from "./config/multer.cjs"
+import authMiddleware from "./app/middlewares/auth.mjs"
 
 const routes = new Router()
 const upload = multer(multerConfig)
 
 routes.post("/users", UserController.store)
 routes.post("/sessions", SessionController.store)
+
+routes.use(authMiddleware)
 routes.post("/products", upload.array("files"), ProductController.store)
 routes.get("/products", ProductController.index)
 
